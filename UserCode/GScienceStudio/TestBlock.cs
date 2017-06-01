48afe1a45414cbf105ce8dd96cc687e1735cc9ab
GM2000 <GM2000@outlook.com>
2017-06-01 21:31:57 +0800

    完善自动生成

--- UserCode/GScienceStudio/TestBlock.cs
+++ UserCode/GScienceStudio/TestBlock.cs
@@ -10,5 +10,22 @@ namespace GScienceStudio
     public class TestBlock : CubeBlock
     {
         public const int Index = 300;
+
+        public override int GetFaceTextureSlot(int face, int value)
+        {
+            if (face == 4)
+            {
+                return 0;
+            }
+            if (face == 5)
+            {
+                return 2;
+            }
+            if (TerrainData.ExtractData(value) == 0)
+            {
+                return 3;
+            }
+            return 0x44;
+        }
     }
 }
