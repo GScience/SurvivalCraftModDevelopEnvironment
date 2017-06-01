d280bdba2cee0c1ad656c0b81dd3f1950b9c34cd
GM2000 <GM2000@outlook.com>
2017-06-01 19:12:19 +0800

    添加测试用Block

---
+++ UserCode/GScienceStudio/TestBlock.cs
@@ -0,0 +1,14 @@
+﻿using Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GScienceStudio
+{
+    public class TestBlock : CubeBlock
+    {
+        public const int Index = 300;
+    }
+}
